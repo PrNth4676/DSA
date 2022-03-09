222c6402698d6be2ed446b9f331d9150a8e341f8
Pratik Nath <pratiknth3@gmail.com>
2022-03-09 09:16:50 +0530

    Reversing a character array using Java

--- DSA/src/arrayProblems/ReverseArray.java
+++ DSA/src/arrayProblems/ReverseArray.java
@@ -7,11 +7,12 @@ public class ReverseArray {
 		for(int i=originalText.length-1;i>=0;i--) {
 			reversedText[(originalText.length-1)-i]=originalText[i];
 		}
+		System.out.println(reversedText);
 		return reversedText;
 	}
 	
-	
 	public static void main(String[] args) {
+		//Original text is as below
 		char[] inputArray= {'r','u','f','u','s'};
 		ReverseArray reverseArray = new ReverseArray();
 		reverseArray.reverseTheInputArray(inputArray);
