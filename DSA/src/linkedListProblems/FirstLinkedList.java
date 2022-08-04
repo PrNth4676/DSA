25a0a36a6c06933b197b26c7b0b9475977d8aadd
Pratik Nath <pratiknth3@gmail.com>
2022-08-04 11:03:46 +0530

    Written code for Linked List in Java

--- DSA/src/linkedListProblems/FirstLinkedList.java
+++ DSA/src/linkedListProblems/FirstLinkedList.java
@@ -2,8 +2,25 @@ package linkedListProblems;
 
 public class FirstLinkedList {
 	
-	public void traverseByGap(Node head, int k) {
-		
+	public int traverseByGap(Node head, int k) {
+		if(head==null) {
+			return -1;
+		}
+		Node p,q;
+		q=head;
+		p=head;
+		k = k-1;
+		while(k-- >=0 && q!=null) {
+			q=q.next;
+		}
+		if(q==null) {
+			return -1;
+		}
+		while(q!=null) {
+			q=q.next;
+			p=p.next;
+		}
+		return p.data;
 	}
 	
 	public static void main(String[] args) {
