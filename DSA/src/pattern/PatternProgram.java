419fe51eccc5e4ab8664dfb558fb83a42396fe38
Pratik Nath <pratiknth3@gmail.com>
2022-08-18 11:05:49 +0530

    Updating the file with two more commands

--- DSA/src/pattern/PatternProgram.java
+++ DSA/src/pattern/PatternProgram.java
@@ -51,11 +51,35 @@ public class PatternProgram {
 		}
 	}
 
+	public void leftDownTriangle(int size) {
+		for(int i=0;i<size;i++) {
+			for(int j=size-1;j>=i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	//REDO AGAIN ON 19-08-2022
+	public void rightDownTriangle(int size) {
+		for(int i=0;i<size;i++) {
+			for(int j=0;j<i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=size;j>i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
 	public static void main(String[] args) {
 		PatternProgram program = new PatternProgram();
 //		program.squarePattern(5);
 //		program.hollowSquare(5);
 //		program.leftTriangleStarPattern(5);
-		program.rightTriangleStarPattern(5);
+//		program.rightTriangleStarPattern(5);
+//		program.leftDownTriangle(5);
+		program.rightDownTriangle(5);
 	}
 }
