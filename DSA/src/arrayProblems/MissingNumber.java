50cd3d18a32fed063daaa9483c69435b00b78475
Pratik Nath <pratiknth3@gmail.com>
2023-02-08 08:49:09 +0530

    Added a solution to problem solved using XOR operation

--- DSA/src/arrayProblems/MissingNumber.java
+++ DSA/src/arrayProblems/MissingNumber.java
@@ -14,6 +14,8 @@ public class MissingNumber {
         return rangeSum-numsSum;
 	}
 	
+	//When we try to store value beyond the range of the data-type then, overflow can happen.
+	
 	//XOR Operation which prevents data-type overflow
 	//Time Complexity - O(n)
 	//Space Complexity - O(1)
@@ -27,9 +29,20 @@ public class MissingNumber {
         return rangeSum^numsSum;
 	}
 	
+	public int singleNumber(int[] nums) {
+		int xorSum=0;
+		for(int i=0;i<nums.length;i++) {
+			xorSum^=nums[i];
+		}
+		return xorSum;
+	}
+	
 	public static void main(String[] args) {
-		int[] nums= {3,0,1};
+//		int[] nums= {3,0,1};
+		int[] nums= {2,2,1};
 		MissingNumber number = new MissingNumber();
-		number.missingNoByXOR(nums);
+//		number.missingNoByXOR(nums);
+//		number.missingNo(nums);
+		number.singleNumber(nums);
 	}
 }
