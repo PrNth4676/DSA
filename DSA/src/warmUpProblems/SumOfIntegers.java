960de10e451cf7e30ca21a775d160730efa2bf97
Pratik Nath <pratiknth3@gmail.com>
2023-12-27 07:21:37 +0530

    Day 2 of Array - DSA

--- DSA/src/warmUpProblems/SumOfIntegers.java
+++ DSA/src/warmUpProblems/SumOfIntegers.java
@@ -9,10 +9,26 @@ public class SumOfIntegers {
 		}
 		return sum;
 	}
+	
+	//Print the Element in Array when key is matched with index
+	public static int printKeyElement(int[] inputIntegers,int key) {
+		int element = 0;
+		if(key>inputIntegers.length) {
+			element = -1;
+		} else {
+			for(int i=0;i<inputIntegers.length;i++) {
+				if(key==(i+1)) {
+					element=inputIntegers[i];
+				}
+			}
+		}
+		return element;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] inputArray = {1,2,3,4};
 		System.out.println(SumOfIntegers.sumOfIntegers(inputArray));
+		System.out.println(SumOfIntegers.printKeyElement(inputArray, 5));
 	}
 }
