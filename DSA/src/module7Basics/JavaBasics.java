fc3f378a57189467239ae4ce49fc1f38dd28f048
Pratik Nath <48478737+PrNth4676@users.noreply.github.com>
2025-04-25 08:06:26 +0530

    User Input in Java

--- DSA/src/module7Basics/JavaBasics.java
+++ DSA/src/module7Basics/JavaBasics.java
@@ -1,5 +1,7 @@
 package module7Basics;
 
+import java.util.Scanner;
+
 public class JavaBasics {
 
 	// VARIABLES
@@ -42,8 +44,22 @@ public class JavaBasics {
 		return Math.round(((double) 4 / 3) * Math.PI * Math.pow(radius, 3));
 	}
 
+	// Simple Interest
+	public double findSI(double principal, double rate, double time) {
+		return (principal * rate * time) / 100;
+	}
+
+	// Power of a number
+	public double findPowerOfNumber(double number, int power) {
+		return Math.pow(number, power);
+	}
+
 	public static void main(String[] args) {
 		JavaBasics basics = new JavaBasics();
+		double radius = 7;
+		double principalAmount = 7000;
+		double rate = 12;
+		double time = 2;
 		System.out.println("Hello World!"); // Prints a new line after the end of message
 		System.out.println("The value of variable i is : " + basics.i);
 		System.out.println("The sum of the variables are : " + (basics.i + basics.j));
@@ -55,8 +71,18 @@ public class JavaBasics {
 				+ basics.updateVariableByDiff(basics.i, 20));
 		System.out.println("The value of variable " + basics.i + " will be updated to : "
 				+ basics.updateVariableByDiv(basics.i, 10.0));
-		double radius = 7;
 		System.out.println("Area of a Circle with radius " + radius + " is : " + basics.findAreaOfCircle(radius));
 		System.out.println("Volume of a Sphere with radius " + radius + " is : " + basics.findVolumeOfSphere(radius));
+		System.out.println("Simple Interest for an amount of " + principalAmount + " over a time period of " + time
+				+ " at the rate of " + rate + " % is : " + basics.findSI(principalAmount, rate, time));
+		System.out.println("The matured amount on the principal amount of " + principalAmount + " will be : "
+				+ (principalAmount + basics.findSI(principalAmount, rate, time)));
+		Scanner scanner = new Scanner(System.in); // Here, "in" means the system will take input from either Keyboard or Mouse
+		System.out.print("Enter the number : ");
+		double number = scanner.nextDouble();
+		System.out.print("Enter the power : ");
+		int power = scanner.nextInt();
+		System.out.println("The number "+number+" raised to power "+power+" is : "+basics.findPowerOfNumber(number, power));
+		scanner.close(); //Closing the scanner to avoid program running indefinitely.
 	}
 }
