f4ee52dd58a28843c6bb89cd6d8693a7c0498d02
Pratik Nath <48478737+PrNth4676@users.noreply.github.com>
2025-05-21 08:04:28 +0530

    Array Operations

--- DSA/src/module12Arrays/ArrayOperations.java
+++ DSA/src/module12Arrays/ArrayOperations.java
@@ -50,6 +50,28 @@ public class ArrayOperations {
 
 	/**
 	 * @author Pratik
+	 * @Summary : Returns the second maximum element in array
+	 **/
+	public static int findSecondMaximumElementInArray() {
+		int max = arr[0];
+		int secondMax = arr[0];
+		for (int i = 1; i < arr.length; ++i) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		for (int i = 1; i < arr.length; ++i) {
+			if (arr[i] != max) { // Filters the maximum element of the array
+				if (arr[i] > secondMax) {
+					secondMax = arr[i];
+				}
+			}
+		}
+		return secondMax;
+	}
+
+	/**
+	 * @author Pratik
 	 * @Summary : Returns the minimum element in array
 	 **/
 	public static int findMinimumElementInArray() {
@@ -63,10 +85,29 @@ public class ArrayOperations {
 		return min;
 	}
 
+	/**
+	 * @author Pratik
+	 * @Summary : Updates an element in Array
+	 **/
+	public static void updateElementInArray(int element, int index) {
+		if (index >= arr.length || index < 0) {
+			System.out.println("Invalid Index");
+		} else {
+			for (int i = 0; i < arr.length; ++i) {
+				arr[index] = element;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		int element = 20;
 		System.out.println("Element " + element + " is present at index : " + returnElementInArray(element));
 		System.out.println("Maximum Element of the Array is : " + findMaximumElementInArray());
 		System.out.println("Minimun Element of the Array is : " + findMinimumElementInArray());
+		System.out.println("Second Maximum Element in the Array is : " + findSecondMaximumElementInArray());
+		updateElementInArray(10, -1);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 }
