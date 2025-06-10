cb43c497f94b7b30adcc7ee93e5149cf1e4c7871
Pratik Nath <48478737+PrNth4676@users.noreply.github.com>
2025-06-10 10:01:03 +0530

    SDL on ArrayList

--- DSA/src/module13multiDArray/ArrayList2DBasics.java
+++ DSA/src/module13multiDArray/ArrayList2DBasics.java
@@ -7,7 +7,7 @@ public class ArrayList2DBasics {
 
 	public static void main(String[] args) {
 
-		// Initialization
+		// Declaration
 		List<List<Integer>> array2DList = new ArrayList<>(); // This will create an empty ArrayList where each element
 																// is an ArrayList
 		int size = 4;
@@ -17,6 +17,45 @@ public class ArrayList2DBasics {
 		List<List<Integer>> sizeArray2DList2 = new ArrayList<>(size);
 		for (int i = 0; i < size; ++i) {
 			sizeArray2DList2.add(new ArrayList<>(arraySize));
+		}
+
+		// Initialization
+		List<Integer> a = new ArrayList<>();
+		a.add(10);
+		a.add(20);
+		a.add(30);
+
+		List<Integer> b = new ArrayList<>();
+		b.add(40);
+		b.add(50);
+		b.add(60);
+
+		List<Integer> c = new ArrayList<>();
+
+		List<Integer> d = new ArrayList<>();
+		d.add(70);
+		d.add(80);
+
+		array2DList.add(a);
+		array2DList.add(b);
+		array2DList.add(c);
+		array2DList.add(d);
+
+		// Fetch elements using For-Each Loop
+		for (List<Integer> l : array2DList) {
+			for (Integer i : l) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+
+		// Fetch elements using Get Function in For Loop
+		for (int i = 0; i < array2DList.size(); ++i) {
+			List<Integer> x = array2DList.get(i);
+			for (int j = 0; j < x.size(); ++j) {
+				System.out.print(x.get(j) + " ");
+			}
+			System.out.println();
 		}
 	}
 }
