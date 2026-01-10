9ee6020ae3b9a837a3dcc6836a9e4e6c13496dde
Pratik Nath <48478737+PrNth4676@users.noreply.github.com>
2026-01-10 07:22:18 +0530

    Add string utility methods and examples
    Introduced several string-related utility methods: isVowel, indexOfChar, compareTo, compareToOwn, and commonFunctionsOfString. Updated main to demonstrate string comparison methods and removed redundant charAt examples.

--- DSA/src/module14Strings/StringBasics.java
+++ DSA/src/module14Strings/StringBasics.java
@@ -13,8 +13,81 @@ public class StringBasics {
 		scanner.close();
 	}
 
-	public static void main(String[] args) {
+	public static boolean isVowel(char ch) {
+		if (ch == 'a' || ch == 'A')
+			return true;
+		if (ch == 'e' || ch == 'E')
+			return true;
+		if (ch == 'i' || ch == 'I')
+			return true;
+		if (ch == 'o' || ch == 'O')
+			return true;
+		if (ch == 'u' || ch == 'U')
+			return true;
+		return false;
+	}
 
+	/**It returns the index of the character in a string**/
+	public static int indexOfChar(String str, char ch) {
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ch) {
+				return i;
+			}
+		}
+		return -1; // char not found
+	}
+	
+	public static void compareTo(String str1, String str2) {
+		// It returns the difference in ASCII values of first non-matching character
+		System.out.println(str1.compareTo(str2));
+	}
+	
+	public static void compareToOwn(String str1, String str2) {
+		int len = Math.min(str1.length(), str2.length());
+		for (int i = 0; i < len; i++) {
+			if (str1.charAt(i) != str2.charAt(i)) {
+				System.out.println(str1.charAt(i) - str2.charAt(i));
+				return;
+			}
+		}
+		// If all characters are same in the min length
+		System.out.println(str1.length() - str2.length());
+	}
+	
+	
+	public static void commonFunctionsOfString(String str) {
+		// length()
+		int length = str.length();
+		System.out.println("Length of string : " + length);
+		
+		// charAt()
+		for (int i = 0; i < length; i++) {
+			System.out.print(str.charAt(i) + " ");
+		}
+		System.out.println();
+		
+		// indexOf()
+		char ch = 'a';
+		int index = indexOfChar(str, ch);
+		if (index != -1) {
+			System.out.println("Character '" + ch + "' found at index: " + index);
+		} else {
+			System.out.println("Character '" + ch + "' not found in the string.");
+		}
+		
+		// contains()
+		boolean containsSubstr = str.contains("sub");
+		System.out.println("String contains 'sub': " + containsSubstr);
+		
+		//startsWith() and endsWith()
+		boolean startsWith = str.startsWith("pre");
+		boolean endsWith = str.endsWith("post");
+		System.out.println("String starts with 'pre': " + startsWith);
+		System.out.println("String ends with 'post': " + endsWith);		
+	}
+	
+
+	public static void main(String[] args) {
 		// String is a char array
 		char[] arr = { 'P', 'R', 'A', 'T', 'I', 'K' };
 		for (char ele : arr) {
@@ -25,11 +98,10 @@ public class StringBasics {
 		String str = "Pratik";
 		System.out.println(str);
 //		collectStringFromUser();
-		
-		System.out.println(str.charAt(4));
-		System.out.println(str.charAt(5));
-		
 		int lengthOfString = str.length();
 		System.out.println(lengthOfString);
+		
+		compareTo("apple", "banana");
+		compareToOwn("apple", "apricot");
 	}
 }
