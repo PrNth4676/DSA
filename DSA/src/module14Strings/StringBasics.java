a8c021fc5f27f3d9bfcfb49d342ccc1dd8806dd5
Pratik Nath <48478737+PrNth4676@users.noreply.github.com>
2026-01-13 10:10:10 +0530

    Add substring methods and string concatenation examples
    Added methods to print substrings and all substrings of a string. Introduced a method demonstrating the use of the plus operator for string concatenation, and updated main to call these new methods for demonstration.

--- DSA/src/module14Strings/StringBasics.java
+++ DSA/src/module14Strings/StringBasics.java
@@ -27,7 +27,7 @@ public class StringBasics {
 		return false;
 	}
 
-	/**It returns the index of the character in a string**/
+	/** It returns the index of the character in a string **/
 	public static int indexOfChar(String str, char ch) {
 		for (int i = 0; i < str.length(); i++) {
 			if (str.charAt(i) == ch) {
@@ -36,12 +36,12 @@ public class StringBasics {
 		}
 		return -1; // char not found
 	}
-	
+
 	public static void compareTo(String str1, String str2) {
 		// It returns the difference in ASCII values of first non-matching character
 		System.out.println(str1.compareTo(str2));
 	}
-	
+
 	public static void compareToOwn(String str1, String str2) {
 		int len = Math.min(str1.length(), str2.length());
 		for (int i = 0; i < len; i++) {
@@ -53,19 +53,18 @@ public class StringBasics {
 		// If all characters are same in the min length
 		System.out.println(str1.length() - str2.length());
 	}
-	
-	
+
 	public static void commonFunctionsOfString(String str) {
 		// length()
 		int length = str.length();
 		System.out.println("Length of string : " + length);
-		
+
 		// charAt()
 		for (int i = 0; i < length; i++) {
 			System.out.print(str.charAt(i) + " ");
 		}
 		System.out.println();
-		
+
 		// indexOf()
 		char ch = 'a';
 		int index = indexOfChar(str, ch);
@@ -74,18 +73,50 @@ public class StringBasics {
 		} else {
 			System.out.println("Character '" + ch + "' not found in the string.");
 		}
-		
+
 		// contains()
 		boolean containsSubstr = str.contains("sub");
 		System.out.println("String contains 'sub': " + containsSubstr);
-		
-		//startsWith() and endsWith()
+
+		// startsWith() and endsWith()
 		boolean startsWith = str.startsWith("pre");
 		boolean endsWith = str.endsWith("post");
 		System.out.println("String starts with 'pre': " + startsWith);
-		System.out.println("String ends with 'post': " + endsWith);		
+		System.out.println("String ends with 'post': " + endsWith);
 	}
-	
+
+	/** Sub-String **/
+	public static void printSubString(String str) {
+		System.out.println(str.substring(2));
+		System.out.println(str.substring(1, 4)); // It will print from 1st to (4-1)th index
+	}
+
+	/**
+	 * @author Pratik
+	 * @Method : It prints all the substrings of a string
+	 **/
+	public static void printAllSubString(String str) {
+		for (int i = 0; i < str.length(); ++i) {
+			for (int j = i + 1; j <= str.length(); ++j) {
+				System.out.print(str.substring(i, j) + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void plusOperatorInString() {
+		String str1 = "Hello";
+//		String str2 = "World";
+		int str2 = 10;
+		String str3 = str1 + " " + str2; // Concatenation
+//		str1 += str2;
+//		System.out.println(str1);
+		System.out.println(str3);
+		System.out.println("abc" + 10 + 20); // abc1020 - left to right evaluation where concatenation happens first
+		// as the first operand is a string and following operands are converted to
+		// string
+		System.out.println(10 + 20 + "abc"); // 30abc - left to right evaluation where addition happens first
+	}
 
 	public static void main(String[] args) {
 		// String is a char array
@@ -100,8 +131,11 @@ public class StringBasics {
 //		collectStringFromUser();
 		int lengthOfString = str.length();
 		System.out.println(lengthOfString);
-		
+
 		compareTo("apple", "banana");
 		compareToOwn("apple", "apricot");
+
+		printAllSubString("abcd");
+		plusOperatorInString();
 	}
 }
